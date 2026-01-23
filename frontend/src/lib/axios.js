52a1331a4d378511b61dcd9216fa18a8bcb7885d
MKSubrahmanya12345 <mksubbu007@gmail.com>
2026-01-23 23:53:48 +0530

    Added Frontend to live Backend and fixed /api endpoint miss

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development" 
     ? "http://localhost:3000/api" 
-    : "https://bloodlink-4edh.onrender.com/", // <--- PASTE RENDER URL HERE
+    : "https://bloodlink-4edh.onrender.com/api", // <--- PASTE RENDER URL HERE
   withCredentials: true,
 });
 
