924bce7b31cd2748ba5d9004a4aca4fc42f4c55b
MKSubrahmanya12345 <mksubbu007@gmail.com>
2026-01-23 23:51:02 +0530

    Added Frontend to live Backend

--- backend/src/lib/axios.js
+++ backend/src/lib/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
+  // If we are in production, use the live URL. If dev, use localhost.
   baseURL: import.meta.env.MODE === "development" 
     ? "http://localhost:3000/api" 
-    : "https://bloodlink-4edh.onrender.com/", // <--- PASTE RENDER URL HERE
+    : "/api", // We will use a proxy on Vercel (cleaner) OR put your full Render URL here
   withCredentials: true,
 });
-
