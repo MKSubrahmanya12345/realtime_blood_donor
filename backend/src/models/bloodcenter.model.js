b8fbdddb7d71b7bc5363018f412e973265370c8b
MKSubrahmanya12345 <mksubbu007@gmail.com>
2026-01-25 17:13:10 +0530

    added blood center page

--- backend/src/models/bloodcenter.model.js
+++ backend/src/models/bloodcenter.model.js
@@ -4,14 +4,14 @@ const bloodCenterSchema = new mongoose.Schema({
   state: { type: String, required: true },
   district: { type: String, required: true },
   city: { type: String, required: true },
-  name: { type: String, required: true },
+  bloodBankName: { type: String, required: true },
   parentHospital: String,
-  contactPerson: { type: String, required: true },
+  contactPerson: String,
   email: { type: String, required: true },
-  phone: { type: String, required: true },
-  licenseNumber: { type: String, required: true },
-  donationTypes: [String], // Array for checkboxes
-  componentTypes: [String],
+  contactNumber: { type: String, required: true },
+  licenseNumber: String,
+  donationTypes: [String], // Array to store multiple checkboxes
+  componentTypes: [String]
 }, { timestamps: true });
 
 export const BloodCenter = mongoose.model("BloodCenter", bloodCenterSchema);
